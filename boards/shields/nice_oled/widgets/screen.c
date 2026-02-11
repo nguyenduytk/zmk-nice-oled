ad94cd68e17227e7168b5af63a7c16bba145e0a5
Trien Nguyen <nguyenduytrien92@gmail.com>
2026-02-11 20:55:34 +0700

    bat mv

--- boards/shields/nice_oled/widgets/screen.c
+++ boards/shields/nice_oled/widgets/screen.c
@@ -923,7 +923,7 @@ static struct battery_status_state battery_status_get_state(const zmk_event_t *e
 
     return (struct battery_status_state){
         .level = (ev != NULL) ? ev->state_of_charge : zmk_battery_state_of_charge(),
-        .bat_mv = (ev != NULL) ? ev->bat_mv : zmk_battery_get_mv(),
+        .bat_mv = zmk_battery_get_mv(),
 #if IS_ENABLED(CONFIG_USB_DEVICE_STACK)
         .usb_present = zmk_usb_is_powered(),
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK) */
